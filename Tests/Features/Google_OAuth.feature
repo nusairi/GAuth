a736151a7e97160a800c650223f8f03217ea4b23
nusairi <nusairi@outlook.com>
2017-10-30 11:01:55 +0000

    Add project files.

---
+++ Tests/Features/Google_OAuth.feature
@@ -0,0 +1,27 @@
+﻿Feature: GoogleTask service 	
+Authentication should be provided
+
+@Sunny @Sanity
+Scenario Outline: Create Task
+Given a valid google user is logged in
+When he requested task "<NewTaskTitle>" creation under taskList "<TaskListName>"
+Then the task should be created
+
+Examples:
+| NewTaskTitle | TaskListName |
+|1|Mir's list|
+|2|Mir's list|
+|3|Mir's list|
+|4|Mir's list|
+
+
+
+@Sunny @Sanity
+Scenario Outline: Move Task
+Given a valid google user is logged in
+When he requested to move the task "<TaskTitle>" under taskList "<TaskListName>"
+Then the task should be moved
+
+Examples:
+| TaskTitle | TaskListName |
+|3|Mir's list      |
---
+++ Tests/StepDefinitions/Google_OAuth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Tests
+{
+    [Binding]
+    public sealed class Google_OAuth
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given(@"a valid google user is logged in")]
+        public void GivenAValidGoogleUserIsLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"he requested task ""(.*)"" creation under taskList ""(.*)""")]
+        public void WhenHeRequestedTaskCreationUnderTaskList(int p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the task should be created")]
+        public void ThenTheTaskShouldBeCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"he requested to move the task ""(.*)"" under taskList ""(.*)""")]
+        public void WhenHeRequestedToMoveTheTaskUnderTaskList(int p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the task should be moved")]
+        public void ThenTheTaskShouldBeMoved()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
